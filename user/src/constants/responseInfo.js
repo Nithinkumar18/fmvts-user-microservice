9df6a0d531e823729f327f781ef18376adf02868
Nithin <vooturinithinkumar@gmail.com>
2025-07-25 21:40:07 +0530

    define info messages

--- user/src/constants/responseInfo.js
+++ user/src/constants/responseInfo.js
@@ -1,4 +1,8 @@
 const info = {
-    SERVICE: "FMAVTS-UserMicroService"
+    SERVICE: "FMAVTS-UserMicroService",
+    USER_REGESTRAION_SUCCESS: "User Regestration successfull",
+    USER_REGESTRAION_FAIL: "User Regestration Failed",
+    USER_NOT_FOUND: "User not found with given Id"
+
 }
 module.exports = info;
